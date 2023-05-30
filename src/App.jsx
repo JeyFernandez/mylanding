b501fbce4e7c40b7ce62c802da56759fa2b8c25b
Jasson Fernandez <jeicobfer@gmail.com>
2023-05-29 23:00:13 -0600

    continue

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import Perfil from './image/jey.jpg'
-import './App.css'
+import Perfil from "./image/jey.jpg";
+import "./App.css";
 function Home() {
   return (
-    <div className="presentations">
-    <div className="imagen">
+    <div className="landing">
+      <div className="perfiles">
         <img src={Perfil} />
-        <h2>Hola, <br /> Soy Jasson Ariel Fernandez</h2>
+        <p>Jasson Fernandez</p>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+        Obcaecati dolore in repellat neque rem omnis quos, saepe molestias
+        nobis? Dolor officiis <br /> ducimus beatae necessitatibus illo
+        voluptatem temporibus quae labore facilis.
+      </p>
     </div>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Obcaecati dolore in repellat neque rem omnis quos, saepe molestias nobis? Dolor officiis <br /> ducimus beatae necessitatibus illo voluptatem temporibus quae labore facilis.
-    </p>
-    </div>
-);
+  );
 }
 
 export default Home;
