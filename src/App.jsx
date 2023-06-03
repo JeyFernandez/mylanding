be1c6e0086ab28a8621fb6d22dab25271cf25038
Jasson Fernandez <jeicobfer@gmail.com>
2023-06-03 11:55:29 -0600

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Perfil from "./image/jey.jpg";
 import Banner from "./image/banner.png";
 import "./App.css";
 import AOS from "aos";
+import Btn from "./components/Btn";
 import "aos/dist/aos.css";
 
 function Home() {
@@ -15,6 +16,7 @@ function Home() {
       <div className="container">
         <div className="perfiles">
           <img data-aos="zoom-in" className="foto" src={Perfil} />
+          <Btn />
         </div>
         <div>
           <p className="info">
--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,14 +1,27 @@
 import { useEffect } from "react";
-import React from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { FaWhatsapp, FaFacebook } from "react-icons/fa";
-import { MdMail } from "react-icons/md";
+import "./Btn.css";
 
- function Btn{
-    return (
+import {
+  FaWhatsapp,
+  FaFacebook,
+  FaTelegram,
+  FaInstagram,
+} from "react-icons/fa";
+import { MdMail } from "react-icons/md";
+import { BsGithub } from "react-icons/bs";
+import { FcContacts, FcApproval } from "react-icons/fc";
+function Btn() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <div>
       <div className="contact">
-        <h2>Contact me </h2>
+        <h2>
+          <FcContacts />
+        </h2>
         {/* btn */}
         <div data-aos="fade-right" type="submit" className="submit">
           <p className="wat">WhatsApp</p>
@@ -19,16 +32,16 @@ import { MdMail } from "react-icons/md";
           </div>
         </div>
         <div data-aos="fade-right" type="submit" className="submit">
-          <p className="face">Telegram</p>
-          <div id="colorface" class="icon">
+          <p className="tel">Telegram</p>
+          <div id="colortel" class="icon">
             <div className="svg">
-              <FaFacebook />
+              <FaTelegram />
             </div>
           </div>
         </div>
         <div data-aos="fade-right" type="submit" className="submit">
-          <p className="face">Correo</p>
-          <div id="colorface" class="icon">
+          <p className="mail">Correo</p>
+          <div id="colormail" class="icon">
             <div className="svg">
               <MdMail />
             </div>
@@ -36,7 +49,47 @@ import { MdMail } from "react-icons/md";
         </div>
         {/* btn */}
       </div>
-    );
-  }
+
+      <div className="contact">
+        <h2>
+          <FcApproval />
+        </h2>
+        {/* btn */}
+        <div data-aos="fade-right" type="submit" className="submit">
+          <p className="git">GitHub</p>
+          <div id="colorgit" class="icon">
+            <div className="svg">
+              <BsGithub />
+            </div>
+          </div>
+        </div>
+        <div data-aos="fade-right" type="submit" className="submit">
+          <p className="face">Facebook</p>
+          <div id="colorface" class="icon">
+            <div className="svg">
+              <FaFacebook />
+            </div>
+          </div>
+        </div>
+        <div data-aos="fade-right" type="submit" className="submit">
+          <p className="insta">Instagram</p>
+          <div id="colorinsta" class="icon">
+            <div className="svg">
+              <FaInstagram />
+            </div>
+          </div>
+        </div>
+        <div data-aos="fade-right" type="submit" className="submit">
+          <p className="mail">Instagram</p>
+          <div id="colormail" class="icon">
+            <div className="svg">
+              <FaInstagram />
+            </div>
+          </div>
+        </div>
+        {/* btn */}
+      </div>
+    </div>
+  );
 }
 export default Btn;
